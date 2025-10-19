f11db290bc84bdc507969dc75088f1c0c2e2d454
Manavoid3 <134713169+Nnbebra@users.noreply.github.com>
2025-10-19 11:41:56 +0200

    Update buy.py

--- buy.py
+++ buy.py
@@ -1,3 +1,4 @@
+# buy.py
 from fastapi import APIRouter, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -5,15 +6,17 @@ from fastapi.templating import Jinja2Templates
 templates = Jinja2Templates(directory="templates")
 router = APIRouter()
 
-# Список тарифов (пока статично, потом можно вынести в БД)
+# Статичные тарифы (можно вынести в БД позднее)
 PLANS = {
-    "30": {"id": "30", "title": "Лицензия на 30 дней", "old_price": 350, "price": 299, "discount": "15%", "img": "/static/products/30days.png"},
-    "90": {"id": "90", "title": "Лицензия на 90 дней", "old_price": 900, "price": 749, "discount": "20%", "img": "/static/products/90days.png"},
-    "365": {"id": "365", "title": "Лицензия на 365 дней", "old_price": 3500, "price": 2399, "discount": "30%", "img": "/static/products/365days.png"},
+    "30":   {"id": "30",   "title": "Лицензия на 30 дней",  "old_price": 350,  "price": 299,  "discount": "15%", "img": "/static/products/30days.png"},
+    "90":   {"id": "90",   "title": "Лицензия на 90 дней",  "old_price": 900,  "price": 749,  "discount": "20%", "img": "/static/products/90days.png"},
+    "365":  {"id": "365",  "title": "Лицензия на 365 дней", "old_price": 3500, "price": 2399, "discount": "30%", "img": "/static/products/365days.png"},
+    "life": {"id": "life", "title": "Лицензия навсегда",    "old_price": 6000, "price": 3999, "discount": "33%", "img": "/static/products/lifetime.png"},
 }
 
 @router.get("/buy", response_class=HTMLResponse)
 async def buy_page(request: Request):
+    # Передаём values(), чтобы удобно итерироваться в шаблоне
     return templates.TemplateResponse("buy.html", {"request": request, "plans": PLANS.values()})
 
 @router.get("/checkout/{plan_id}", response_class=HTMLResponse)
